d0a01cec84118251cd3c561f1c460b3a020a069e
Allen <wukuohao2022@icloud.com>
2025-12-01 03:20:49 +0800

    -

--- lua/synapse/config.lua
+++ lua/synapse/config.lua
@@ -8,7 +8,7 @@ M.default = {
 		default = "OriginCoderPulse/synapse.nvim",
 		package_path = os.getenv("HOME") .. "/.synapse/package",
 		config_path = os.getenv("HOME") .. "/.config/nvim",
-		load_config = os.getenv("HOME") .. "/.config/nvim/lua/",
+		load_config = os.getenv("HOME") .. "/.config/nvim",
 
 		ui = {
 			style = "float",
